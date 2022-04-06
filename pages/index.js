d2e82c8737236892ca4c7546fc7ab7b122d6ee2e
LeeroyKiplagat <leeroykiplagat@gmail.com>
2022-04-06 16:42:51 +0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,41 @@
+
+import Head from 'next/head'
+import Sidebar from '../components/Sidebar'
+import Center from '../components/Center'
+import Player from '../components/Player'
+import { getSession } from 'next-auth/react'
+
+function Home() {
+  return (
+    <div className=" bg-black h-screen overflow-hidden">
+      <Head>
+        <title>Spotify2.0</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className='flex'>
+        <Sidebar/>
+        <Center />
+      </main>
+
+      <div className='sticky bottom-0'>
+        <Player/>
+      </div>
+
+      
+    </div>
+  )
+}
+
+export default Home
+
+export async function getServerSideProps(context){
+  const session=await getSession(context);
+
+  return{
+    props:{
+      session,
+    }
+  }
+
+}
