4e070450d33b8467ecfc328cd4192f6798188a3e
LeeroyKiplagat <leeroykiplagat@gmail.com>
2022-04-10 00:37:29 +0300

    final

--- pages/login.js
+++ pages/login.js
@@ -2,7 +2,7 @@ import {getProviders, signIn} from "next-auth/react";
 
 function Login({providers}) {
   return (
-    <div className="flex flex-col items-center justify-center bg-black ">
+    <div className="flex flex-col items-center justify-center bg-black h-screen ">
        <img className="w-52 mb-5" src="https://links.papareact.com/9xl" alt="" />
        {Object.values(providers).map((provider)=>(
            <div key={provider.name} className="">
